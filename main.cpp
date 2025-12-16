ce88ffddba537388dbb0bf07a7291afed113a5d6
Musab81 <musabzubari@gmail.com>
2025-12-16 22:26:50 +0500

    Data structures

--- main.cpp
+++ main.cpp
@@ -29,3 +29,45 @@ const int screenHeight = 720;
 const int canvasWidth = 850;
 const int panelX = canvasWidth + 1;
 const int panelWidth = screenWidth - canvasWidth - 1;
+
+// ============================================================================
+// DATA STRUCTURES
+// ============================================================================
+struct DataPoint {
+    double time{ 0.0 };
+    double position_x{ 0.0 };
+    double position_y{ 0.0 };
+    double velocity_x{ 0.0 };
+    double velocity_y{ 0.0 };
+    double kinetic_energy{ 0.0 };
+    double potential_energy{ 0.0 };
+    double pressure{ 0.0 };
+};
+
+struct Ball {
+    double x{ 0.0 }, y{ 0.0 };
+    double vx{ 0.0 }, vy{ 0.0 };
+    double radius{ 0.5 }, mass{ 1.0 };
+    int id{ 0 };
+};
+
+struct ExperimentParameters {
+    string experiment_type = "CollisionBalls";
+    double gravity = 9.81;
+    double air_resistance = 0.05;
+    double mass = 2.0;
+    double length = 2.0;
+    double initial_angle = 45.0;
+    double spring_constant = 50.0;
+    double ball_masses[2] = { 2.0, 1.5 };
+    double ball_radii[2] = { 0.6, 0.5 };
+    double initial_velocities_x[2] = { 4.0, -3.0 };
+    double initial_velocities_y[2] = { 0.0, 0.0 };
+    double fluid_density = 1000.0;
+    double pipe_diameter1 = 0.15;
+    double pipe_diameter2 = 0.06;
+    double fluid_velocity = 2.0;
+    double static_pressure = 101325.0;
+    double projectile_speed = 25.0;
+    double projectile_angle = 45.0;
+};
